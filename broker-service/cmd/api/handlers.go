e7501234fd439a17918aa3fcd5b40c309213d87b
Fabien D <fabien.ducret@hotmail.fr>
2023-04-22 13:54:11 +0200

    [CHORE] Simplify broker

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -7,12 +7,10 @@ import (
 )
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
-	payload := jsonResponse{
+	app.writeJSON(w, http.StatusOK, jsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
-	}
-
-	app.writeJSON(w, http.StatusOK, payload)
+	})
 }
 
 func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
@@ -26,31 +24,55 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 
 	switch requestPayload.Action {
 	case "auth":
-		asr := app.Container.AuthenticationServiceRepository
-		payload, err := Authenticate(asr, requestPayload.Auth)
-		if err != nil {
-			app.errorJSON(w, err, http.StatusUnauthorized)
-			return
-		}
-
-		app.writeJSON(w, http.StatusAccepted, payload)
+		app.handleAuthenticate(w, requestPayload)
 	case "log":
-		lr := app.Container.LoggerRepository
-		payload, err := Log(lr, requestPayload.Log)
-		if err != nil {
-			app.errorJSON(w, err)
-		}
-
-		app.writeJSON(w, http.StatusAccepted, payload)
+		app.handleLog(w, requestPayload)
 	case "mail":
-		mr := app.Container.MailerRepository
-		payload, err := SendMail(mr, requestPayload.Mail)
-		if err != nil {
-			app.errorJSON(w, err)
-		}
-
-		app.writeJSON(w, http.StatusAccepted, payload)
+		app.handleMail(w, requestPayload)
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
+}
+
+func (app *Config) handleAuthenticate(w http.ResponseWriter, requestPayload ports.RequestPayload) {
+	asr := app.Container.AuthenticationServiceRepository
+	reply, err := asr.AuthenticateWith(ports.Credentials(requestPayload.Auth))
+	if err != nil {
+		app.errorJSON(w, err, http.StatusUnauthorized)
+		return
+	}
+
+	app.writeJSON(w, http.StatusAccepted, jsonResponse{
+		Error:   false,
+		Message: "Authenticated !",
+		Data:    reply,
+	})
+}
+
+func (app *Config) handleLog(w http.ResponseWriter, requestPayload ports.RequestPayload) {
+	lr := app.Container.LoggerRepository
+	reply, err := lr.Log(ports.Log(requestPayload.Log))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	app.writeJSON(w, http.StatusAccepted, jsonResponse{
+		Error:   false,
+		Message: reply,
+	})
+}
+
+func (app *Config) handleMail(w http.ResponseWriter, requestPayload ports.RequestPayload) {
+	mr := app.Container.MailerRepository
+	reply, err := mr.Send(ports.Mail(requestPayload.Mail))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	app.writeJSON(w, http.StatusAccepted, jsonResponse{
+		Error:   false,
+		Message: reply,
+	})
 }
