989c97a68b93acef9b29a988c50b22346c256d75
Fabien D <fabien.ducret@hotmail.fr>
2023-04-28 17:38:28 +0200

    [CHORE] Improve readibility

--- authentication-service/db/postgres.go
+++ authentication-service/db/postgres.go
@@ -9,25 +9,20 @@ import (
 
 func Connect() *sql.DB {
 	dsn := os.Getenv("DSN")
-	retries := 0
 	maxRetries := 5
 
-	for {
-		db, err := openDB(dsn)
+	for i := 0; i < maxRetries; i++ {
+		db, _ := openDB(dsn)
 		if db != nil {
 			log.Println("Connected to Postgres")
 			return db
 		}
 
-		if retries > maxRetries {
-			log.Println(err)
-			return nil
-		}
-
 		log.Println("Postgres not yet ready, sleep 2 seconds...")
 		sleepTwoSeconds()
-		retries++
 	}
+
+	return nil
 }
 
 func openDB(dsn string) (*sql.DB, error) {
